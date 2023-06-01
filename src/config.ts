f1cd4ee726e778cf4b9b0c06c4228b13849751b0
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-06-02 01:07:46 +1200

    mongo connection + express server

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,18 @@
 import dotenv from "dotenv"
 dotenv.config()
-const { CLIENT_ID, GUILD_ID, TOKEN } = process.env;
-if(!CLIENT_ID || !GUILD_ID || !TOKEN) {
+const { CLIENT_ID, GUILD_ID, TOKEN, MONGO_URI, PORT } = process.env;
+if(!CLIENT_ID || !GUILD_ID || !TOKEN || !MONGO_URI) {
     throw new Error("Missing environment variables")
 }
+
+const SERVER_PORT = PORT || "1337";
 
 const config: Record<string, string> = {
     CLIENT_ID,
     GUILD_ID,
-    TOKEN
+    TOKEN,
+    MONGO_URI,
+    SERVER_PORT,
 }
 
 export default config
