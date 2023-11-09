61d569225a2bf29954aad93345594c566de93c0d
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-11-10 04:45:52 +1300

    Restructured cron code

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import './bot';
-
-// muting event creation pings.
-
-// import './webhook/server';
-// import './create-channel'
--- src/services/scheduler.ts
+++ src/services/scheduler.ts
@@ -6,6 +6,42 @@ import weeklySync from '../scheduledMessages/weeklySync';
 import announceEvent from '../scheduledMessages/announceEvent';
 import Logging from '../library/Logging';
 
+const eventsCronJob = async () => {
+  try {
+    const events = await calendar.getNextEvents();
+
+    if (!events.length) return;
+
+    events.forEach((event: calendar_v3.Schema$Event) => {
+      announceEvent(event);
+    });
+  } catch (e) {
+    Logging.error(e);
+  }
+};
+
+const syncCronJob = async () => {
+  try {
+    const event = await calendar.getWeeklySync();
+
+    if (!event) return;
+
+    let scheduledTime = new Date(event.start!.dateTime as string);
+
+    const weeksPassed = Math.floor(
+      (new Date().getTime() - scheduledTime.getTime()) /
+        (7 * 24 * 60 * 60 * 1000)
+    );
+    scheduledTime = new Date(
+      scheduledTime.getTime() + (weeksPassed + 1) * 7 * 24 * 60 * 60 * 1000
+    );
+
+    weeklySync(event, scheduledTime);
+  } catch (e) {
+    Logging.error(e);
+  }
+};
+
 function runScheduler() {
   //updates local sheets cache every 6 hours
   new CronJob('0 */6 * * *', () => sheets.writeName, null, true);
@@ -13,29 +49,7 @@ function runScheduler() {
   // quarter hourly refresh to fetch for upcoming events, and schedule them to run.
   new CronJob(
     '*/15 * * * *', // runs every 15 minutes
-    async () => {
-      try {
-        const events = await calendar.getNextEvents();
-
-        if (!events.length) return;
-
-        events.forEach((event: calendar_v3.Schema$Event) => {
-          let scheduledTime = new Date(event.start!.dateTime as string);
-          // calculates the time to send out the message based on the start time of weekly recurring events
-          announceEvent(event);
-
-          if (event.summary === '💻 Weekly Sync') {
-            // sends the announcement 1 hour before
-            scheduledTime.setHours(scheduledTime.getHours() - 1);
-          } else {
-            // sends the event out 24 hours before it starts
-            scheduledTime.setDate(scheduledTime.getDate() - 1);
-          }
-        });
-      } catch (e) {
-        Logging.error(e);
-      }
-    },
+    eventsCronJob,
     null,
     true
   );
@@ -43,27 +57,7 @@ function runScheduler() {
   // quarter hourly refresh to fetch for weekly syncs.
   new CronJob(
     '*/15 * * * *', // runs every 15 minutes
-    async () => {
-      try {
-        const event = await calendar.getWeeklySync();
-
-        if (!event) return;
-
-        let scheduledTime = new Date(event.start!.dateTime as string);
-
-        const weeksPassed = Math.floor(
-          (new Date().getTime() - scheduledTime.getTime()) /
-            (7 * 24 * 60 * 60 * 1000)
-        );
-        scheduledTime = new Date(
-          scheduledTime.getTime() + (weeksPassed + 1) * 7 * 24 * 60 * 60 * 1000
-        );
-
-        weeklySync(event, scheduledTime);
-      } catch (e) {
-        Logging.error(e);
-      }
-    },
+    syncCronJob,
     null,
     true
   );
